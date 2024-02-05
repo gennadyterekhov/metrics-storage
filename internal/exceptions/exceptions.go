dd168eda6f32d3970c1dd9cdb6c00ba84b5248e0
Gennady Terekhov <ter.ge@yandex.ru>
2024-02-05 20:33:06 +0400

    iter3

--- internal/exceptions/exceptions.go
+++ internal/exceptions/exceptions.go
@@ -7,6 +7,7 @@ const EmptyMetricName = "empty metric name"
 const EmptyMetricValue = "empty metric value"
 const UnknownMetricType = "type is 'unknown'"
 const UnknownError = "unknown error"
+const UnknownMetricName = "unknown Metric Name"
 
 const UpdateMetricsMethodNotAllowed = "Method not allowed. Allowed: POST"
 const GetOneMetricsMethodNotAllowed = "Method not allowed. Allowed: GET"
