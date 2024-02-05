4ddaa627fc0c22d084a12a1e195a8392ee52c221
Gennady Terekhov <ter.ge@yandex.ru>
2024-02-05 20:45:52 +0400

    iter3

--- internal/repositories/metrics_repository.go
+++ internal/repositories/metrics_repository.go
@@ -6,6 +6,8 @@ type MetricsRepository interface {
 
 	GetGauge(name string) (float64, error)
 	GetCounter(name string) (int64, error)
+	GetGaugeOrZero(name string) float64
+	GetCounterOrZero(name string) int64
 
 	GetAllGauges() map[string]float64
 	GetAllCounters() map[string]int64
