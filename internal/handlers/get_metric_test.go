d4128d37d8a6ccc9ed22f7dac966d05e3539b837
Gennady Terekhov <ter.ge@yandex.ru>
2024-02-05 21:10:19 +0400

    iter3

--- internal/handlers/get_metric_test.go
+++ internal/handlers/get_metric_test.go
@@ -1,7 +1,10 @@
 package handlers
 
 import (
+	"context"
 	"github.com/gennadyterekhov/metrics-storage/internal/container"
+	"github.com/gennadyterekhov/metrics-storage/internal/types"
+	"github.com/go-chi/chi/v5"
 	"github.com/stretchr/testify/assert"
 	"io"
 	"net/http"
@@ -12,12 +15,14 @@ import (
 
 func TestGetMetric(t *testing.T) {
 	container.Instance.MetricsRepository.AddCounter("cnt", 1)
+
 	type want struct {
 		code        int
 		metricValue int64
 	}
 	type args struct {
-		url string
+		typ  string
+		name string
 	}
 	tests := []struct {
 		name string
@@ -25,30 +30,34 @@ func TestGetMetric(t *testing.T) {
 		want want
 	}{
 		{
-			name: "",
-			args: args{url: "/value/counter/cnt"},
+			name: "ok",
+			args: args{typ: types.Counter, name: "cnt"},
 			want: want{code: http.StatusOK, metricValue: 1},
 		},
 		{
-			name: "",
-			args: args{url: "/value/counter/unknown"},
+			name: "name unknown",
+			args: args{typ: types.Counter, name: "unknown"},
 			want: want{code: http.StatusNotFound, metricValue: 0},
 		},
 		{
-			name: "",
-			args: args{url: "/value/unknown/cnt"},
+			name: "type unknown",
+			args: args{typ: "unknown", name: "cnt"},
 			want: want{code: http.StatusNotFound, metricValue: 0},
 		},
 		{
-			name: "",
-			args: args{url: "/value/counter/"},
+			name: "name empty",
+			args: args{typ: types.Counter, name: ""},
 			want: want{code: http.StatusNotFound, metricValue: 0},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 
-			request := httptest.NewRequest(http.MethodGet, tt.args.url, nil)
+			url := "/value/" + tt.args.typ + "/" + tt.args.name
+			request := httptest.NewRequest(http.MethodGet, url, nil)
+			rctx := chi.NewRouteContext()
+			rctx.URLParams.Add("key", "value")
+			request = request.WithContext(context.WithValue(request.Context(), chi.RouteCtxKey, rctx))
 			w := httptest.NewRecorder()
 			GetMetric(w, request)
 
