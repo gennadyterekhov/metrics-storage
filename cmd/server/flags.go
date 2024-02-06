8dd07d601a2a7d0573d62bd10649bbbd597c6a5e
Gennady Terekhov <ter.ge@yandex.ru>
2024-02-06 23:18:06 +0400

    iter5

--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -3,9 +3,14 @@ package main
 import (
 	"flag"
 	"github.com/gennadyterekhov/metrics-storage/internal/cliargs"
+	"os"
 )
 
 func parseFlags() string {
+	rawAddress, ok := os.LookupEnv("ADDRESS")
+	if ok {
+		return rawAddress
+	}
 	netAddressFlag := new(cliargs.NetAddress)
 	_ = flag.Value(netAddressFlag)
 	flag.Var(netAddressFlag, "a", "Net address host:port")
