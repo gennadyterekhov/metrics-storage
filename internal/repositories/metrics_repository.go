507ea68eb4e8d4d6721930ed2c12e4a0a4267e05
Gennady Terekhov <ter.ge@yandex.ru>
2024-02-09 23:07:45 +0400

    review fix

--- internal/repositories/metrics_repository.go
+++ internal/repositories/metrics_repository.go
@@ -4,8 +4,6 @@ type MetricsRepository interface {
 	GetGauge(name string) (float64, error)
 	GetCounter(name string) (int64, error)
 
-	GetAllGauges() map[string]float64
-	GetAllCounters() map[string]int64
 	GetAll() (map[string]float64, map[string]int64)
 
 	SetGauge(name string, value float64)
