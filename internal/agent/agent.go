1d1d4820de6168f97ca343dd0c59d77b4f0a2697
Gennady Terekhov <ter.ge@yandex.ru>
2024-02-10 23:59:33 +0400

    after review fix

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -28,6 +28,12 @@ func RunAgent(config *AgentConfig) (err error) {
 	}
 
 	for i := 0; ; i += 1 {
+
+		// TODO fix interval issue
+		// У нас pollInterval 2с, reportInterval 10с
+		// Какой будет метрика PollCount на сервере через 20с?
+		// Условно мы 10 раз сделали poll и 2 раза репорт.
+		// В идеальном мире(все операции моментальны) она должна бы быть равна 10, а будет?
 		time.Sleep(time.Second)
 
 		if !pollerInstance.IsRunning && i%config.PollInterval == 0 {
