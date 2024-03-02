9dddc490e3866488a0d43c25c8148ed214267a39
Gennady Terekhov <ter.ge@yandex.ru>
2024-03-02 17:50:56 +0400

    iter8

---
+++ internal/server/httpui/middleware/compressor/compressor_test.go
@@ -0,0 +1,97 @@
+package compressor
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type args struct {
+	accepts          []string
+	acceptEncodings  []string
+	contentEncodings []string
+	contenttype      string
+}
+
+func TestIsGzipAvailableForThisRequest(t *testing.T) {
+	// in tests this wants gzip
+	//        Accept: application/json
+	//        Accept-Encoding: gzip
+	//        Content-Type: application/json
+	// and sometimes
+	// Accept: html/text
+
+	tests := []struct {
+		name string
+		args args
+		ok   bool
+	}{
+		{
+			name: "only json",
+			args: args{contenttype: "json"},
+			ok:   false,
+		},
+		{
+			name: "only html/text",
+			args: args{accepts: []string{"asdf", "html/text", "asdf"}},
+			ok:   false,
+		},
+		{
+			name: " html/text gzip",
+			args: args{accepts: []string{"asdf", "html/text", "asdf"},
+				acceptEncodings: []string{"asdf", "gzip", "asdf"}},
+			ok: true,
+		},
+		{
+			name: " text/html gzip",
+			args: args{accepts: []string{"asdf", "text/html", "asdf"},
+				acceptEncodings: []string{"asdf", "gzip", "asdf"}},
+			ok: true,
+		},
+		{
+			name: " json gzip",
+			args: args{accepts: []string{"asdf", "application/json", "asdf"},
+				acceptEncodings: []string{"asdf", "gzip", "asdf"}},
+			ok: true,
+		},
+		{
+			name: "json gzip gzip",
+			args: args{accepts: []string{"asdf", "application/json", "asdf"},
+				acceptEncodings:  []string{"asdf", "gzip", "asdf"},
+				contentEncodings: []string{"asdf", "gzip", "asdf"}},
+			ok: true,
+		},
+		{
+			name: "only contentEncodings",
+			args: args{contentEncodings: []string{"asdf", "gzip", "asdf"}},
+			ok:   true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			request := httptest.NewRequest("", "", nil)
+			addHeaders(request, tt.args)
+
+			ok := IsGzipAvailableForThisRequest(request)
+			assert.Equal(t, tt.ok, ok)
+
+		})
+	}
+}
+
+func addHeaders(request *http.Request, args args) {
+	for _, val := range args.accepts {
+		request.Header.Set("Accept", val)
+	}
+	for _, val := range args.acceptEncodings {
+		request.Header.Set("Accept-Encoding", val)
+	}
+	for _, val := range args.contentEncodings {
+		request.Header.Set("Content-Encoding", val)
+	}
+	if args.contenttype != "" {
+		request.Header.Set("Content-Type", args.contenttype)
+	}
+}
