2315e4f24596dc10c69e9bcdf6e18e27365a9486
Gennady Terekhov <ter.ge@yandex.ru>
2024-03-03 16:50:28 +0400

    iter9

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,13 +3,26 @@ package main
 import (
 	"fmt"
 	"github.com/gennadyterekhov/metrics-storage/internal/server/httpui/handlers"
+	"github.com/gennadyterekhov/metrics-storage/internal/server/storage"
 	"net/http"
 )
 
 func main() {
-	address := getAddress()
-	fmt.Printf("Server started on %v\n", address)
-	err := http.ListenAndServe(address, handlers.GetRouter())
+	config := getConfig()
+	fmt.Printf("Server started on %v\n", config.Addr)
+
+	if config.FileStorage != "" {
+		if config.Restore {
+			err := storage.MetricsRepository.Load(config.FileStorage)
+			if err != nil {
+				panic(err)
+				return
+			}
+		}
+		defer storage.MetricsRepository.Save(config.FileStorage)
+	}
+
+	err := http.ListenAndServe(config.Addr, handlers.GetRouter())
 	if err != nil {
 		panic(err)
 	}
