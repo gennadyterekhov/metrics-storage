d788ef564462d4b889f6ad07e2e1a0853637e51e
Gennady Terekhov <ter.ge@yandex.ru>
2024-03-03 15:57:46 +0400

    iter9

--- internal/server/storage/disk.go
+++ internal/server/storage/disk.go
@@ -1,1 +1,28 @@
 package storage
+
+import (
+	"encoding/json"
+	"os"
+)
+
+func (settings MemStorage) Save(fname string) error {
+	// сериализуем структуру в JSON формат
+	data, err := json.MarshalIndent(settings, "", "   ")
+	if err != nil {
+		return err
+	}
+	// сохраняем данные в файл
+	return os.WriteFile(fname, data, 0666)
+}
+
+func (settings *MemStorage) Load(fname string) error {
+	fbytes, err := os.ReadFile(fname)
+	if err != nil {
+		return err
+	}
+	// прочитайте файл с помощью os.ReadFile
+	// десериализуйте данные используя json.Unmarshal
+	// ...
+	tempSettings := &MemStorage{}
+	json.Unmarshal(fbytes, tempSettings)
+}
--- internal/server/storage/ram.go
+++ internal/server/storage/ram.go
@@ -6,38 +6,38 @@ import (
 )
 
 type MemStorage struct {
-	counters map[string]int64
-	gauges   map[string]float64
+	Counters map[string]int64
+	Gauges   map[string]float64
 }
 
 func CreateStorage() *MemStorage {
 	return &MemStorage{
-		counters: make(map[string]int64, 0),
-		gauges:   make(map[string]float64, 0),
+		Counters: make(map[string]int64, 0),
+		Gauges:   make(map[string]float64, 0),
 	}
 }
 
 func (strg *MemStorage) Clear() {
-	strg.counters = make(map[string]int64, 0)
-	strg.gauges = make(map[string]float64, 0)
+	strg.Counters = make(map[string]int64, 0)
+	strg.Gauges = make(map[string]float64, 0)
 }
 
 func (strg *MemStorage) HasGauge(name string) bool {
-	_, ok := strg.gauges[name]
+	_, ok := strg.Gauges[name]
 	return ok
 }
 
 func (strg *MemStorage) HasCounter(name string) bool {
-	_, ok := strg.counters[name]
+	_, ok := strg.Counters[name]
 	return ok
 }
 
 func (strg *MemStorage) AddCounter(key string, value int64) {
-	strg.counters[key] += value
+	strg.Counters[key] += value
 }
 
 func (strg *MemStorage) SetGauge(key string, value float64) {
-	strg.gauges[key] = value
+	strg.Gauges[key] = value
 }
 
 func (strg *MemStorage) GetGauge(name string) (float64, error) {
@@ -55,7 +55,7 @@ func (strg *MemStorage) GetCounter(name string) (int64, error) {
 }
 
 func (strg *MemStorage) GetGaugeOrZero(name string) float64 {
-	val, ok := strg.gauges[name]
+	val, ok := strg.Gauges[name]
 	if !ok {
 		return 0
 	}
@@ -63,7 +63,7 @@ func (strg *MemStorage) GetGaugeOrZero(name string) float64 {
 }
 
 func (strg *MemStorage) GetCounterOrZero(name string) int64 {
-	val, ok := strg.counters[name]
+	val, ok := strg.Counters[name]
 	if !ok {
 		return 0
 	}
@@ -71,11 +71,11 @@ func (strg *MemStorage) GetCounterOrZero(name string) int64 {
 }
 
 func (strg *MemStorage) GetAllGauges() map[string]float64 {
-	return strg.gauges
+	return strg.Gauges
 }
 
 func (strg *MemStorage) GetAllCounters() map[string]int64 {
-	return strg.counters
+	return strg.Counters
 }
 
 func (strg *MemStorage) GetAll() (map[string]float64, map[string]int64) {
